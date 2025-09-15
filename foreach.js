17d78f83ba7f52ff4b6b51b21a73f01aa82e3775
BLK Garut <you@example.com>
2025-09-15 09:00:03 +0700

    Foreach

---
+++ foreach.js
@@ -0,0 +1,24 @@
+var angka = [1,2,3,4,5,6,7,8];
+// angka.forEach(function(e){
+//     console.log(e);
+// });
+
+
+// angka.map(function(e){
+//     return e;
+// });
+
+// var angka2 = angka.map(function(e){
+//     return e*2;
+// });
+// console.log(angka2.join('-'));
+
+let angka  = [1,2,3,4,5];
+
+angka.forEach(function(e){
+    console.log(e);
+});
+
+let angka = [1,2,3,4,5];
+angka.forEach(num => console.log(num *2));
+console.log(angka);
